209437cb13e2b5487c3c248d79b7aba47cf1f1ed
maksa988 <maks.best5@mail.ru>
2017-12-01 21:43:08 +0200

    base

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : Unit
+{
+    [SerializeField]
+    private int lives = 5;
+
+    [SerializeField]
+    private float speed = 3.0F;
+    [SerializeField]
+    private float jumpForce = 15.0F;
+
+    private bool isGrounded = false;
+
+    private CharState State
+    {
+        get { return (CharState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
+
+    new private Rigidbody2D rigidbody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    private void Update()
+    {
+        if (isGrounded) State = CharState.Idle;
+        
+        if (Input.GetButton("Horizontal")) Run();
+        if (isGrounded && Input.GetButtonDown("Jump")) Jump();
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("TP");
+            Vector3 zero = transform.position;
+            zero.x = 2;
+            zero.y = 2;
+            
+        }
+    }
+
+    private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+        sprite.flipX = direction.x < 0.0F;
+
+        if (isGrounded) State = CharState.Run;
+    }
+
+    private void Jump()
+    {
+        rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 1.3F);
+
+        isGrounded = colliders.Length > 1;
+
+        if (!isGrounded) State = CharState.Jump;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        
+    }
+}
+
+
+public enum CharState
+{
+    Idle,
+    Run,
+    Jump
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public bool avaliable = true;
+    public Transform target;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(avaliable)
+        {
+            other.gameObject.transform.position = target.position;
+            Camera.main.transform.position = target.position;
+        }
+    }
+}
