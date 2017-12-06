5dac78c86cc2b494ab4e3afc1c1b5a966de49860
Zaza <lizamelnyk@gmail.com>
2017-12-06 23:03:57 +0200

    updating menu
    writing scripts for menu and normalazed graphics

---
+++ Assets/Scripts/Loading_game.js
@@ -0,0 +1,22 @@
+﻿//menu battons
+var Play = false;
+var Info = false;
+var Exit = false;
+
+//cameras
+var camera1:Camera;
+var camera2:Camera;
+
+function OnMouseUp() {
+    
+    if (Play == true) {
+        Application.LoadLevel(1);
+    }
+    if (Info == true) {
+        camera1.enabled = false;
+        camera2.enabled = true;
+    }
+    if (Exit == true) {
+        Application.Quit();
+    }
+}
